a11faa1a187ae479ca706c7e7f7b86e9c3eca19b
fineowl <fineowl@users.noreply.github.com>
2016-08-19 13:42:58 -0400

    Blue Star Fire Light

--- Security/Shield22/Shield21/Shield1/Shield0/Blue Shine Star Light.cs
+++ Security/Shield22/Shield21/Shield1/Shield0/Blue Shine Star Light.cs
@@ -1,9 +1,9 @@
 0
 HolyHaO0
 Shield0
-c = 1.634;
+c = 1.622;
 Blue Shine Fire Light
-12 22 28 222 228 2222 2222Autumn
+12 22 28 222 228 2222 2222Autumn 2222Fall 2222Summer
 The Holy Hand
 Abraham Sont
 C256HelixOwl
